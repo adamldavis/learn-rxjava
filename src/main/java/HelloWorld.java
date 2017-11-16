0b1baa3a60cc9ec0e1ffd2bb6c0c1c966fa9dcca
Adam Davis <adavis@thesdg.com>
2017-11-16 10:03:39 -0500

    Added writeFile and readFile

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,8 @@ import io.reactivex.*;
 
 import learn_rxjava.*;
 
+import java.io.File;
+
 public class HelloWorld {
     public static void main(String[] args) {
         try {
@@ -13,7 +15,11 @@ public class HelloWorld {
             
             Flowable.just(Flows.doParallelSquares()).subscribe(System.out::println);
     
+            File f = new File("test");
+            Flows.writeFile(f);
             Thread.sleep(1000); // allows computation to finish
+
+            Flows.readFile(f);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/learn_rxjava/Flows.java
+++ src/main/java/learn_rxjava/Flows.java
@@ -1,7 +1,12 @@
 package learn_rxjava;
 
-import io.reactivex.*;
+import io.reactivex.Flowable;
 import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.io.*;
 import java.util.*;
 
 public class Flows {
@@ -9,9 +14,11 @@ public class Flows {
     public static List doSquares() {
         List squares = new ArrayList();
         Flowable.range(1, 64)
-          .observeOn(Schedulers.computation())
-          .map(v -> v * v)
-          .blockingSubscribe(squares::add);
+            .observeOn(Schedulers.computation())
+            .map(v -> v * v)
+            .doOnError(ex -> ex.printStackTrace())
+            .doOnComplete(() -> System.out.println("Completed doSquares"))
+            .blockingSubscribe(squares::add);
         
         return squares;
     }
@@ -24,6 +31,8 @@ public class Flows {
                 .subscribeOn(Schedulers.computation())
                 .map(w -> w * w)
             )
+            .doOnError(ex -> ex.printStackTrace())
+            .doOnComplete(() -> System.out.println("Completed doParallelSquares"))
             .blockingSubscribe(squares::add);
             
         return squares;
@@ -35,12 +44,74 @@ public class Flows {
             Thread.sleep(1000); //  imitate expensive computation
             return "Done";
         });
+        source.doOnComplete(() -> System.out.println("Completed runComputation"));
+
         Flowable<String> runBackground = source.subscribeOn(Schedulers.io());
 
         Flowable<String> showForeground = runBackground.observeOn(Schedulers.single());
 
         showForeground.subscribe(System.out::println, Throwable::printStackTrace);
-        
+
         //Thread.sleep(2000);
+    }
+
+    public static void writeFile(File file) {
+        try (PrintWriter pw = new PrintWriter(file)) {
+            Flowable.range(1, 100)
+                .observeOn(Schedulers.io())
+                .blockingSubscribe(pw::println);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readFile(File file) {
+        try (final BufferedReader pw = new BufferedReader(new FileReader(file))) {
+
+            Flowable<String> flowable = Flowable.fromPublisher(new FilePublisher(pw));
+
+            flowable.observeOn(Schedulers.io())
+                    .blockingSubscribe(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class FilePublisher implements Publisher<String> {
+        BufferedReader reader;
+        public FilePublisher(BufferedReader reader) { this.reader = reader; }
+        @Override
+        public void subscribe(Subscriber<? super String> subscriber) {
+            subscriber.onSubscribe(new FilePublisherSubscription(this, subscriber));
+        }
+        public String readLine() throws IOException {
+            return reader.readLine();
+        }
+    }
+
+    static class FilePublisherSubscription implements Subscription {
+        FilePublisher publisher;
+        Subscriber<? super String> subscriber;
+        public FilePublisherSubscription( FilePublisher pub, Subscriber<? super String> sub) {
+            this.publisher = pub;
+            this.subscriber = sub;
+        }
+        @Override
+        public void request(long n) {
+            try {
+                String line;
+                for (int i = 0; i < n && publisher != null && (line = publisher.readLine()) != null; i++) {
+                    if (subscriber != null) subscriber.onNext(line);
+                }
+            } catch (IOException ex) {
+                subscriber.onError(ex);
+            }
+            subscriber.onComplete();
+        }
+        @Override
+        public void cancel() {
+            publisher = null;
+        }
     }
 }
